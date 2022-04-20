ce97bd4db12d5c6b603bd390df172fc795b9552d
ArianaMesquita <arianamesquita85@gmail.com>
2022-04-20 16:16:47 -0300

    First commit

---
+++ src/DioJava/PrimeiroDioJava.java
@@ -0,0 +1,17 @@
+package DioJava;
+
+import DioJava.model.gato;
+
+public class PrimeiroDioJava {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+        /*        int a = 5;
+        int b = 3;
+        System.out.println("Hello World!! " + (a+b));*/
+
+
+    }
+}
